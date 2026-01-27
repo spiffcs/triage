e97085fe8989184a4acc9d0e797ac4d05a7ce221
Christopher Phillips <spiffcs@users.noreply.github.com>
2026-01-27 16:35:32 -0500

    chore: fix Body.Close
    Signed-off-by: Christopher Phillips <spiffcs@users.noreply.github.com>

--- internal/github/graphql.go
+++ internal/github/graphql.go
@@ -222,7 +222,7 @@ func (c *Client) executeGraphQL(query string, token string) (json.RawMessage, er
 	if err != nil {
 		return nil, fmt.Errorf("GraphQL request failed: %w", err)
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
